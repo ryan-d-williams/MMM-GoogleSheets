5d29f51d68a1f576326e3b9f1d0877c780c943d6
Ryan Williams <ryan.d.williams.94@gmail.com>
2024-08-08 20:32:14 -0400

    Updated docs for node helper

--- node_helper.js
+++ node_helper.js
@@ -3,19 +3,7 @@
   Node Helper for MMM-GoogleSheets.
 
   This helper is responsible for the data pull from Google Apps Script
-  At a minimum the API key, Latitude and Longitude parameters
-  must be provided.  If any of these are missing, the request
-  to Dark Sky will not be executed, and instead an error
-  will be output the the MagicMirror log.
-
-  Additional, this module supplies two optional parameters:
-
-    units - one of "ca", "uk2", "us", or "si"
-    lang - Any of the languages Dark Sky supports, as listed here: https://darksky.net/dev/docs#response-format
-
-  The Dark Sky API request looks like this:
-
-    https://api.darksky.net/forecast/API_KEY/LATITUDE,LONGITUDE?units=XXX&lang=YY
+  At a minimum the sheet and range must be provided.
 
 *********************************/
 
