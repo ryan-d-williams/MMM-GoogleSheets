3b955846b7d022b854923730f068bd12654533f0
Ryan Williams <ryan.d.williams.94@gmail.com>
2024-10-20 21:58:55 -0400

    Moved helper functions to their own classes. Added basic scrolling code

--- MMM-GoogleSheets.js
+++ MMM-GoogleSheets.js
@@ -8,7 +8,6 @@
   MIT Licensed
 
 *********************************/
-
 Module.register("MMM-GoogleSheets", {
   requiresVersion: "2.2.0",
 
@@ -91,18 +90,21 @@ Module.register("MMM-GoogleSheets", {
       range: false
     };
 
+	this.tableScrolling = new TableScrolling();
+	this.dataProcessing = new GoogleSheetsDataProcessing(this.config);
+
     // Sanitize required parameters
-    if (!this.config.hasOwnProperty("url") || this.config.url.length == 0) {
+    if (!Object.hasOwn(this.config, "url") || this.config.url.length == 0) {
       this.errors.url = true;
       this.paramErrors = true;
     }
 
-    if (!this.config.hasOwnProperty("sheet") || this.config.sheet.length == 0) {
+    if (!Object.hasOwn(this.config, "sheet") || this.config.sheet.length == 0) {
       this.errors.sheet = true;
       this.paramErrors = true;
     }
 
-    if (!this.config.hasOwnProperty("range") || this.config.range.length == 0) {
+    if (!Object.hasOwn(this.config, "range") || this.config.range.length == 0) {
       this.errors.range = true;
       this.paramErrors = true;
     }
@@ -130,7 +132,7 @@ Module.register("MMM-GoogleSheets", {
         "updateFadeSpeed"
       ]);
 
-      var self = this;
+      let self = this;
       setTimeout(function () {
         //first data pull is delayed by config
         self.getData();
@@ -154,7 +156,14 @@ Module.register("MMM-GoogleSheets", {
     });
   },
 
-  socketNotificationReceived: function (notification, payload) {
+  notificationReceived: async function (notification){
+    if (notification == "DOM_OBJECTS_UPDATED") {
+	  this.tableScrolling.doScrolling(this.sheetData.length);
+    }
+  },
+
+  socketNotificationReceived: async function (notification, payload) {
+    console.log("HERE: " + notification);
     if (
       notification == "GOOGLE_SHEETS_DATA" &&
       payload.instanceId == this.identifier
@@ -167,12 +176,12 @@ Module.register("MMM-GoogleSheets", {
         this.runtimeErrorMsg = payload.error_msg;
         this.updateDom(this.config.updateFadeSpeed);
       } else {
-        let combinedData = this.combineSheetsData(payload);
-        let dataWithMerges = this.processMerges(
+        let combinedData = this.dataProcessing.combineSheetsData(payload);
+        let dataWithMerges = this.dataProcessing.processMerges(
           combinedData,
           payload.merge_data
         );
-        this.sheetData = this.processCellStyles(dataWithMerges);
+        this.sheetData = this.dataProcessing.processCellStyles(dataWithMerges);
 
         this.updateDom(this.config.updateFadeSpeed);
       }
@@ -181,140 +190,13 @@ Module.register("MMM-GoogleSheets", {
     }
   },
 
-  combineSheetsData: function (sheetData) {
-    let data = sheetData.values.map((row, i) => {
-      return row.map((col, j) => {
-        return {
-          value: col,
-          background_color: sheetData.backgrounds[i][j],
-          color: sheetData.font_colors[i][j],
-          text_decoration: sheetData.font_lines[i][j],
-          font_style: sheetData.font_styles[i][j],
-          font_size: sheetData.font_sizes[i][j],
-          font_weight: sheetData.font_weights[i][j],
-          text_align: sheetData.horiz_align[i][j],
-          vertical_align: sheetData.vert_align[i][j],
-          height: sheetData.cell_sizes[i][j].height,
-          width: sheetData.cell_sizes[i][j].width,
-          merge: false,
-          display: true
-        };
-      });
-    });
-
-    return data;
-  },
-
-  processCellStyles: function (data) {
-    data.forEach((row, i) => {
-      row.forEach((col, j) => {
-        col.style = "";
-
-        if (this.config.cellStyle === "flat") {
-          //col.style = "padding: 5px 5px 5px 0px;"
-        } else if (this.config.cellStyle === "invert") {
-          col.style = "color: " + col.background_color + ";";
-        } else if (this.config.cellStyle === "text") {
-          col.style = "color: " + col.color + ";";
-        }
-
-        if (this.config.cellStyle === "mimic") {
-          col.style = `
-			background-color: ${col.background_color};
-			color: ${col.color};
-			text-decoration: ${col.text_decoration};
-			font-style: ${col.font_style};
-			font-size: ${col.font_size}pt;
-			font-weight: ${col.font_weight};
-			text-align: ${col.text_align};
-			vertical-align: ${col.vertical_align};
-			height: ${col.height}px;
-			max-height: ${col.height}px;
-			width: ${col.width}px;
-			max-width: ${col.width}px;
-		`;
-
-          if (col.font_size * 1.333 > col.height) {
-            col.style += "line-height:" + col.font_size * 1.2 + "px;";
-          } else {
-            col.style += "line-height:" + col.font_size + "pt;";
-          }
-
-          col.data_style = `text-align:${col.horiz_align};padding: 0.2em 0.07em;`;
-        } else {
-          col.data_style = "padding: 2px 8px";
-        }
-
-        this.config.stylesFromSheet.forEach((style) => {
-          let suffix = "";
-          if (style === "font-size") {
-            suffix = "pt";
-          } else if (style === "width" || style === "height") {
-            suffix = "px";
-          }
-          col.style +=
-            style + ":" + col[style.replace(/-/g, "_")] + suffix + ";";
-        });
-
-        col.style += this.config.customStyles.join(";") + ";";
-
-        if (i === 0) {
-          col.style += this.config.headerStyles.join(";") + ";";
-        }
-
-        if (this.config.styleFunc) {
-          col.style += this.config.styleFunc(i, j, col);
-        }
-
-        if (this.config.border === "dimmed") {
-          col.style += "border: 1px solid #666;";
-        } else if (this.config.border === "normal") {
-          col.style += "border: 1px solid #999;";
-        } else if (this.config.border === "bright") {
-          col.style += "border: 1px solid #fff;";
-        } else if (this.config.border !== "none") {
-          col.style += "border:" + this.config.border + ";";
-        }
-      });
-    });
-
-    return data;
-  },
-
-  processMerges: function (data, merges) {
-    merges.forEach((merge) => {
-      let row = merge.start_row;
-      let col = merge.start_col;
-      data[row][col].merge = true;
-      data[row][col].merge_data = {
-        colspan: merge.num_cols,
-        rowspan: merge.num_rows
-      };
-
-      let cum_height = 0;
-      for (let i = row; i < row + merge.num_rows; i++) {
-        let cum_width = 0;
-        for (let j = col; j < col + merge.num_cols; j++) {
-          data[i][j].display = false;
-          cum_width += data[i][j].width;
-        }
-        data[row][col].width = cum_width;
-
-        cum_height += data[i][col].height;
-      }
-      data[row][col].height = cum_height;
-    });
-
-    return data;
-  },
-
   /*
     For any config parameters that are expected as integers, this
     routine ensures they are numbers, and if they cannot be
     converted to integers, then the module defaults are used.
    */
   sanitizeNumbers: function (keys) {
-    var self = this;
+    let self = this;
     keys.forEach(function (key) {
       if (isNaN(parseInt(self.config[key]))) {
         self.config[key] = self.defaults[key];
@@ -324,3 +206,197 @@ Module.register("MMM-GoogleSheets", {
     });
   }
 });
+
+class TableScrolling {
+	constructor() {
+		this.currentRowScroll = 6;
+		this.fullTableLength = 0;
+	}
+
+	async doScrolling(fullTableLength){
+	  this.fullTableLength = fullTableLength;
+	  this.setTableHeight(5);
+	  while(true){
+		  if(this.currentRowScroll > this.fullTableLength){
+			  this.currentRowScroll = 5;
+		  }
+
+		  await this.scrollToRow(this.currentRowScroll, 1000)
+		  this.currentRowScroll++;
+		  await new Promise(resolve => setTimeout(resolve, 5000));
+	  }
+	}
+
+	setTableHeight(row_num) {
+	  let wrapper_el = document.getElementsByClassName("table-wrapper")[0];
+	  let row_el =
+		document.getElementsByClassName("sheets-table-row")[row_num - 1];
+
+	  wrapper_el.style.maxHeight = `${row_el.offsetHeight + row_el.offsetTop}px`;
+	}
+
+	async scrollToRow(row_num, duration) {
+	  let wrapper_el = document.getElementsByClassName("table-wrapper")[0];
+	  let row_el =
+		document.getElementsByClassName("sheets-table-row")[row_num - 1];
+
+	  const totalScrollDistance =
+		row_el.offsetTop -
+		wrapper_el.offsetHeight +
+		row_el.offsetHeight -
+		wrapper_el.scrollTop;
+	  let scrollY = wrapper_el.scrollTop;
+	  let newScrollTop = totalScrollDistance + scrollY;
+	  let oldTimestamp = document.timeline.currentTime;
+
+	  await new Promise((resolve) => {
+		requestAnimationFrame(resolve);
+	  });
+
+	  while (scrollY < newScrollTop) {
+		let newTimestamp = document.timeline.currentTime;
+		scrollY +=
+		  (totalScrollDistance * (newTimestamp - oldTimestamp)) / duration;
+		wrapper_el.scrollTop = scrollY;
+		oldTimestamp = newTimestamp;
+
+		await new Promise((resolve) => {
+		  requestAnimationFrame(resolve);
+		});
+	  }
+	  wrapper_el.scrollTop = newScrollTop;
+	}
+  }
+
+class GoogleSheetsDataProcessing {
+	constructor(config) {
+		this.config = config;
+	}
+
+	combineSheetsData (sheetData) {
+		let data = sheetData.values.map((row, i) => {
+		  return row.map((col, j) => {
+			return {
+			  value: col,
+			  background_color: sheetData.backgrounds[i][j],
+			  color: sheetData.font_colors[i][j],
+			  text_decoration: sheetData.font_lines[i][j],
+			  font_style: sheetData.font_styles[i][j],
+			  font_size: sheetData.font_sizes[i][j],
+			  font_weight: sheetData.font_weights[i][j],
+			  text_align: sheetData.horiz_align[i][j],
+			  vertical_align: sheetData.vert_align[i][j],
+			  height: sheetData.cell_sizes[i][j].height,
+			  width: sheetData.cell_sizes[i][j].width,
+			  merge: false,
+			  display: true
+			};
+		  });
+		});
+
+		return data;
+	  }
+
+	  processCellStyles (data) {
+		data.forEach((row, i) => {
+		  row.forEach((col, j) => {
+			col.style = "";
+
+			if (this.config.cellStyle === "flat") {
+			  //col.style = "padding: 5px 5px 5px 0px;"
+			} else if (this.config.cellStyle === "invert") {
+			  col.style = "color: " + col.background_color + ";";
+			} else if (this.config.cellStyle === "text") {
+			  col.style = "color: " + col.color + ";";
+			}
+
+			if (this.config.cellStyle === "mimic") {
+			  col.style = `
+				background-color: ${col.background_color};
+				color: ${col.color};
+				text-decoration: ${col.text_decoration};
+				font-style: ${col.font_style};
+				font-size: ${col.font_size}pt;
+				font-weight: ${col.font_weight};
+				text-align: ${col.text_align};
+				vertical-align: ${col.vertical_align};
+				height: ${col.height}px;
+				max-height: ${col.height}px;
+				width: ${col.width}px;
+				max-width: ${col.width}px;
+			`;
+
+			  if (col.font_size * 1.333 > col.height) {
+				col.style += "line-height:" + col.font_size * 1.2 + "px;";
+			  } else {
+				col.style += "line-height:" + col.font_size + "pt;";
+			  }
+
+			  col.data_style = `text-align:${col.horiz_align};padding: 0.2em 0.07em;`;
+			} else {
+			  col.data_style = "padding: 2px 8px";
+			}
+
+			this.config.stylesFromSheet.forEach((style) => {
+			  let suffix = "";
+			  if (style === "font-size") {
+				suffix = "pt";
+			  } else if (style === "width" || style === "height") {
+				suffix = "px";
+			  }
+			  col.style +=
+				style + ":" + col[style.replace(/-/g, "_")] + suffix + ";";
+			});
+
+			col.style += this.config.customStyles.join(";") + ";";
+
+			if (i === 0) {
+			  col.style += this.config.headerStyles.join(";") + ";";
+			}
+
+			if (this.config.styleFunc) {
+			  col.style += this.config.styleFunc(i, j, col);
+			}
+
+			if (this.config.border === "dimmed") {
+			  col.style += "border: 1px solid #666;";
+			} else if (this.config.border === "normal") {
+			  col.style += "border: 1px solid #999;";
+			} else if (this.config.border === "bright") {
+			  col.style += "border: 1px solid #fff;";
+			} else if (this.config.border !== "none") {
+			  col.style += "border:" + this.config.border + ";";
+			}
+		  });
+		});
+
+		return data;
+	  }
+
+	  processMerges (data, merges) {
+		merges.forEach((merge) => {
+		  let row = merge.start_row;
+		  let col = merge.start_col;
+		  data[row][col].merge = true;
+		  data[row][col].merge_data = {
+			colspan: merge.num_cols,
+			rowspan: merge.num_rows
+		  };
+
+		  let cum_height = 0;
+		  for (let i = row; i < row + merge.num_rows; i++) {
+			let cum_width = 0;
+			for (let j = col; j < col + merge.num_cols; j++) {
+			  data[i][j].display = false;
+			  cum_width += data[i][j].width;
+			}
+			data[row][col].width = cum_width;
+
+			cum_height += data[i][col].height;
+		  }
+		  data[row][col].height = cum_height;
+		});
+
+		return data;
+	  }
+}
